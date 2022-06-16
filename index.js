29dd4ba5f9f4e8f31d08cd75cf5d8bf3e483543c
GYNFLO <gynflo@gmail.com>
2022-06-16 10:08:44 +0200

    add mongose for node connection

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const port = process.env.PORT || 3000;
 
 const app = express();
+
+mongoose
+    .connect("mongodb://gynflo:mypassword@mongo:27017/?authSource=admin")
+    .then(() => console.log('Successfully connected to DB'))
+    .catch((e) => console.log(e));
+
 
 app.get('/', (_, res) => {
     res.send('Everything is ok !!!');
