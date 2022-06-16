666c99dad92e7a043667ab95e5dc2167976506f7
GYNFLO <gynflo@gmail.com>
2022-06-16 10:58:23 +0200

    fix start mongo before node

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require('./config/config');
 const port = process.env.PORT || 3000;
 
 const app = express();
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
+const connectAndRetry = () => {
+    mongoose
+        .connect(mongoURL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        .then(() => console.log('Successfully connected to DB'))
+        .catch((e) => {
+            console.log(e);
+            setTimeout(connectAndRetry, 5000)
+        });
+}
 
-mongoose
-    .connect("mongodb://gynflo:mypassword@mongo:27017/?authSource=admin")
-    .then(() => console.log('Successfully connected to DB'))
-    .catch((e) => console.log(e));
+connectAndRetry();
+
 
 
 app.get('/', (_, res) => {
